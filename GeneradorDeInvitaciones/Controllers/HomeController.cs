6884a568a4eaa94ca5aa0ca1179fa903a0dafce0
emanuel miranda <emanuel.miranda.c@gmail.com>
2018-10-10 16:33:55 -0300

    Commit de prueba

---
+++ GeneradorDeInvitaciones/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GeneradorDeInvitaciones.Models;
+
+namespace GeneradorDeInvitaciones.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public String index_mod() {
+
+            return "Prueba";
+        }
+
+        public ViewResult index() {
+            String saludo;
+            ViewBag.saludo = "Invitación al taller COMO AMARTE A TI MISMO impartido por " +
+                "Marcelo González Pizarro";
+            return View();
+        }
+
+        public ViewResult RsvpForm() {
+            return View();
+        }
+
+        [HttpPost] //Metodo post
+        public ViewResult RsvpForm(RespuestaInvitado respuestaInvitado) {
+
+            if (ModelState.IsValid)
+            {
+                return View("Gracias", respuestaInvitado);
+
+            }
+            else {
+
+                return View();
+            }
+        }
+
+    }
+}
---
+++ GeneradorDeInvitaciones/Models/RespuestaInvitado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneradorDeInvitaciones.Models
+{
+    public class RespuestaInvitado
+    {
+        [Required(ErrorMessage = "Por favor ingrese un nombre")]
+        public String nombre { get; set; }
+        [Required(ErrorMessage = "Por favor ingrese un correo electrónico")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Por favor ingrese correo válido")]
+        public String correo { get; set; }
+        [Required(ErrorMessage = "Por favor ingrese teléfono")]
+        public String telefono { get; set; } 
+        [Required(ErrorMessage = "Por favor confirme asistencia")]
+        public bool? asistira { get; set; }
+    }
+}
